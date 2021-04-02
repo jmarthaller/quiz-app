eadb797c3b259988f39e7d5185d06185d576cfd7
Jacob Marthaller <marthaller.jr@gmail.com>
2021-04-02 09:59:03 -0700

    fetch from API

---
+++ src/API.ts
@@ -0,0 +1,18 @@
+
+
+export enum Difficulty {
+    EASY = "easy",
+    MEDIUM = "medium",
+    HARD = "hard",
+}
+
+
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+    const data = await (await fetch(endpoint)).json();
+    console.log(data);
+}
+
+
+
+// https://opentdb.com/api.php?amount=10&category=24&type=multiple
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 
 type Props = {
     question: string;
-    answer: string[];
+    answers: string[];
     callback: any;
-    userAnswer: string;
+    userAnswer: any;
     questionNumber: number;
     totalQuestions: number;
 }
 
-const QuestionCard: React.FC<Props> = ({ question, answer, callback, userAnswer, questionNumber, totalQuestions }) => (
+const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNumber, totalQuestions }) => (
     
     <div>
       <p className="number">Question: {questionNumber} / {totalQuestions}</p>;
-      <p></p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+          {answers.map(answer => (
+              <div>
+                <button disabled={userAnswer} onClick={callback}>
+                    <span dangerouslySetInnerHTML={{ __html: answer }} />
+                </button>
+              </div>
+          ))}
+      </div>
     </div>
     
     );
