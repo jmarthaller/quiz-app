83036ed36a2ec4ddcb34321901dac076d803f339
Jacob Marthaller <marthaller.jr@gmail.com>
2021-04-02 11:51:59 -0700

    start styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import QuestionCard from './components/QuestionCard';
 import { fetchQuizQuestions } from './API';
 import { QuestionState, Difficulty } from './API';
 
-type AnswerObject = {
+export type AnswerObject = {
   question: string;
   answer: string;
   correct: boolean;
   correctAnswer: string;
 }
 
-const TOTAL_QUESTIONS = 10;
+const TOTAL_QUESTIONS = 10
 
 const App = () => {
   const [loading, setLoading] = useState(false);
@@ -18,7 +18,7 @@ const App = () => {
   const [number, setNumber] = useState(0);
   const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
-  const [gameOver, setGameOver] = useState(true)
+  const [gameOver, setGameOver] = useState(true);
 
   console.log(questions)
 
@@ -36,11 +36,31 @@ const App = () => {
   }
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      const answer = e.currentTarget.value;
 
+      const correct = questions[number].correct_answer === answer;
+
+      if (correct) setScore(prev => prev + 1);
+
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers(prev => [...prev, answerObject])
+    }
   }
 
   const nextQuestion = () => {
+    const nextQuestion = number + 1;
 
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
   }
   
   return (
@@ -51,17 +71,24 @@ const App = () => {
           <button className="start" onClick={startTrivia}>Start</button> 
         ) : null
       }
-      {  !gameOver ? <p className="score">Score: </p> : null  }
-      <p className="loading">Loading Questions...</p>
-      {/* <QuestionCard 
-        questionNumber={number + 1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={userAnswers ? userAnswers[number] : undefined }
-        callback={checkAnswer}
-      /> */}
-      <button className="next" onClick={nextQuestion}>Next Question</button>
+      {  !gameOver ? <p className="score">Score: {score}</p> : null  }
+      { loading && <p className="loading">Loading Questions...</p> }
+      {!loading! && !gameOver && (
+
+        <QuestionCard
+          questionNumber={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined }
+          callback={checkAnswer}
+        />
+
+      )}
+      { !gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
+        <button className="next" onClick={nextQuestion}>Next Question</button>
+      ) : null }
+
     </div>
   );
 }
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { AnswerObject } from '../App';
 
 type Props = {
     question: string;
     answers: string[];
-    callback: any;
-    userAnswer: any;
+    callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    userAnswer: AnswerObject | undefined;
     questionNumber: number;
     totalQuestions: number;
 }
@@ -12,12 +13,12 @@ type Props = {
 const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNumber, totalQuestions }) => (
     
     <div>
-      <p className="number">Question: {questionNumber} / {totalQuestions}</p>;
+      <p className="number">Question: {questionNumber} / {totalQuestions}</p>
       <p dangerouslySetInnerHTML={{ __html: question }} />
       <div>
           {answers.map(answer => (
-              <div>
-                <button disabled={userAnswer} onClick={callback}>
+              <div key={answer}>
+                <button disabled={!!userAnswer} value={answer} onClick={callback}>
                     <span dangerouslySetInnerHTML={{ __html: answer }} />
                 </button>
               </div>
