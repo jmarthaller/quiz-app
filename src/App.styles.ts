a9e2be22702b3dc0ff47b64dc56a769d443de695
Jacob Marthaller <marthaller.jr@gmail.com>
2021-04-02 12:05:33 -0700

    style header

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -14,4 +14,42 @@ body {
     display: flex;
     justify-content: center;
 }
-`
+
+* {
+    box-sizing: border-box;
+    font-family: 'Catamaran', sans-serif;
+}
+
+`;
+
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    > p {
+        color: #fff;
+    }
+
+    .score  { 
+      color: #fff;
+      font-size: 2rem;
+      margin: 0;
+    }
+
+    h1 {
+        font-family: Fascinate Inline, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+        background-image: linear-gradient(180deg, #fff, #87f1ff);
+        background-size: 100%;
+        background-clip: text;
+        --webkit-background-clip: text;
+        --webkit-text-fill-color: color;
+        --moz-background-clip: text;
+        --moz-text-fill-color: transparent;
+        filter: drop-shadow(2px 2px #0085a3);
+        font-size: 70px;
+        text-align: center;
+    }
+
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import QuestionCard from './components/QuestionCard';
 import { fetchQuizQuestions } from './API';
 import { QuestionState, Difficulty } from './API';
+import { GlobalStyle } from './App.styles';
 
 export type AnswerObject = {
   question: string;
@@ -64,32 +65,35 @@ const App = () => {
   }
   
   return (
-    <div className="App">
-      <h1>TYPESCRIPT QUIZ</h1>
-      {
-        gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
-          <button className="start" onClick={startTrivia}>Start</button> 
-        ) : null
-      }
-      {  !gameOver ? <p className="score">Score: {score}</p> : null  }
-      { loading && <p className="loading">Loading Questions...</p> }
-      {!loading! && !gameOver && (
+    <>
+      <GlobalStyle />
+      <div className="App">
+        <h1 className="heading">TYPESCRIPT QUIZ</h1>
+        {
+          gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+            <button className="start" onClick={startTrivia}>Start</button> 
+          ) : null
+        }
+        {  !gameOver ? <p className="score">Score: {score}</p> : null  }
+        { loading && <p className="loading">Loading Questions...</p> }
+        {!loading! && !gameOver && (
 
-        <QuestionCard
-          questionNumber={number + 1}
-          totalQuestions={TOTAL_QUESTIONS}
-          question={questions[number].question}
-          answers={questions[number].answers}
-          userAnswer={userAnswers ? userAnswers[number] : undefined }
-          callback={checkAnswer}
-        />
+          <QuestionCard
+            questionNumber={number + 1}
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswers ? userAnswers[number] : undefined }
+            callback={checkAnswer}
+          />
 
-      )}
-      { !gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
-        <button className="next" onClick={nextQuestion}>Next Question</button>
-      ) : null }
+        )}
+        { !gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
+          <button className="next" onClick={nextQuestion}>Next Question</button>
+        ) : null }
 
-    </div>
+      </div>
+    </>
   );
 }
 
