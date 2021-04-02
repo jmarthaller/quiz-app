6511bb826b622843ed39fdeb21d51887cdd5e5fc
Jacob Marthaller <marthaller.jr@gmail.com>
2021-04-02 10:38:00 -0700

    pass fetch data to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
 import React, { useState } from 'react';
 import QuestionCard from './components/QuestionCard';
 import { fetchQuizQuestions } from './API';
-import { Difficulty } from './API';
+import { QuestionState, Difficulty } from './API';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([false]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true)
 
-  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
+  console.log(questions)
 
   const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
 
+    const newQuestions = await fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY);
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
   }
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -30,8 +46,12 @@ const App = () => {
   return (
     <div className="App">
       <h1>TYPESCRIPT QUIZ</h1>
-      <button className="start" onClick={startTrivia}>Start</button>
-      <p className="score">Score: </p>
+      {
+        gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+          <button className="start" onClick={startTrivia}>Start</button> 
+        ) : null
+      }
+      {  !gameOver ? <p className="score">Score: </p> : null  }
       <p className="loading">Loading Questions...</p>
       {/* <QuestionCard 
         questionNumber={number + 1}
