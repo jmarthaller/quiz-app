0ea0d141c6f6d88a36d7c33a7be6a242778fb29a
Jacob Marthaller <marthaller.jr@gmail.com>
2021-04-02 09:31:36 -0700

    basic component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import QuestionCard from './components/QuestionCard';
 
 
 
-function App() {
+const App = () => {
+  
+  const startTrivia = async () => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+  
   return (
     <div className="App">
-      Quiz App 
+      <h1>TYPESCRIPT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>Start</button>
+      <p className="score">Score: </p>
+      <p className="loading">Loading Questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
 }
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
+type Props = {
+    question: string;
+    answer: string[];
+    callback: any;
+    userAnswer: string;
+    questionNumber: number;
+    totalQuestions: number;
+}
 
-
-const QuestionCard = () => <div>Questions Card</div>;
+const QuestionCard: React.FC<Props> = ({ question, answer, callback, userAnswer, questionNumber, totalQuestions }) => (
+    
+    <div>
+      <p className="number">Question: {questionNumber} / {totalQuestions}</p>;
+      <p></p>
+    </div>
+    
+    );
 
 
 
