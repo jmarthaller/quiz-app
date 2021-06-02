7d6feebd99299e232bc5454e3260791e2034e16b
Jacob Marthaller <marthaller.jr@gmail.com>
2021-06-02 09:14:41 -0700

    Clean up demo files

--- clean-code.js
+++ clean-code.js
@@ -1,19 +1,8 @@
-let x: number = 100;
-let y: string = "Jacob";
+var x = 100;
+var y = "Jacob";
 console.log(typeof x);
 console.log(typeof y);
-
 x = 1000;
 y = "false";
 console.log(typeof x);
 console.log(typeof y);
-
-
-
-
-
-
-
-
-
-
--- messy-code.js
+++ messy-code.js
@@ -1,12 +1,12 @@
-let x = 100;
-let y = "Jacob";
-console.log(typeof x);
-console.log(typeof y);
+// let x = 100;
+// let y = "Jacob";
+// console.log(typeof x);
+// console.log(typeof y);
 
-x = undefined;
-y = false;
-console.log(typeof x);
-console.log(typeof y);
+// x = undefined;
+// y = false;
+// console.log(typeof x);
+// console.log(typeof y);
 
 
 
