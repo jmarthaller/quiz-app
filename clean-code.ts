a3e13ddc04ea24c9ea41e8729a1c781d5449817d
Jacob Marthaller <marthaller.jr@gmail.com>
2022-08-30 17:58:01 -0700

    Delete cruft

--- clean-code.ts
+++ clean-code.ts
@@ -1,9 +1,7 @@
 let x: number = 100;
 let y: string = "Jacob";
-console.log(typeof x);
-console.log(typeof y);
+
 
 x = 1000;
 y = "false";
-console.log(typeof x);
-console.log(typeof y);
+
--- src/App.tsx
+++ src/App.tsx
@@ -22,9 +22,6 @@ const App = () => {
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
-  console.log(questions)
-
-
   const startTrivia = async () => {
     setLoading(true);
     setGameOver(false);
